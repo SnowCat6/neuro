c343facb01dcb2995be36e78a29a1f3ec3db0ed7
snowcat <snowcat@neurotech.lt>
2018-12-21 17:46:55 +0500

    Update enter asks

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ io_config   = io(config_file)
 
 asks    = io_config.asks()
 
+print("Empty to end")
 while True:
-    print("Empty to end")
     ask_value= input("New ask [" + io_config.new_ask_index() + "]: ")
     if ask_value == "":
         break
